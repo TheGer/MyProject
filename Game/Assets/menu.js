1c4ddc935eefb937409b48c014c9a04001b1e559
Gerry Said <gerrysaid@gmail.com>
2014-02-18 09:16:39 +0100

    Added a menu with a button
    I created a new project and added a menu with a single button.

---
+++ Game/Assets/menu.js
@@ -0,0 +1,21 @@
+﻿#pragma strict
+//similar to a CSS style sheet for the button
+//on the screen
+var style:GUISkin;
+
+//creating a function to draw the button
+function OnGUI ()
+{
+	GUI.skin = style;
+	//create a button in the middle of the screen
+	GUI.Button(Rect(400-50,300-50,100,100),"Play Game");
+}
+
+
+function Start () {
+
+}
+
+function Update () {
+
+}
